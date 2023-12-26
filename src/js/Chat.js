85d52d14b1d8770acd0c92335e96d2a17f2c0379
Anastasia <tryes9856@gmail.com>
2023-12-26 15:33:09 +0300

    remove comments

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -15,12 +15,6 @@ export default class Chat {
     this.onGetMessage = this.onGetMessage.bind(this);
     this.onSendMessage = this.onSendMessage.bind(this);
     this.onLogOut = this.onLogOut.bind(this);
-
-    // if (window.performance.getEntriesByType) {
-    //   if (window.performance.getEntriesByType("navigation")[0].type === "reload") {
-    //     this.onLogOut()
-    //   }
-    // }
   }
 
   createModal() {
@@ -108,14 +102,17 @@ export default class Chat {
     li.append(info);
 
     if (data.userName === this.currentUser.name) {
-      info.textContent = `You, ${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${date.getMonth() + 1
-        }.${date.getFullYear()}`;
+      info.textContent = `You, ${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${
+        date.getMonth() + 1
+      }.${date.getFullYear()}`;
       info.closest(".messages__item").classList.add("messages__item_right");
       info.classList.add("messages__info_right");
     } else {
-      info.textContent = `${data.userName
-        }, ${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${date.getMonth() + 1
-        }.${date.getFullYear()}`;
+      info.textContent = `${
+        data.userName
+      }, ${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${
+        date.getMonth() + 1
+      }.${date.getFullYear()}`;
       info.closest(".messages__item").classList.add("messages__item_left");
     }
 
